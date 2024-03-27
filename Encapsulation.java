b479b764f721160c63736c6272a55f18a3b3fd25
003-Aman <amanshrestha3003@gmail.com>
2024-03-27 00:32:05 -0400

    encapsulation

---
+++ Encapsulation.java
@@ -0,0 +1,24 @@
+/* 
+ * packages and access modifiers
+ * different types of packages like for color, data modification , different packages have different uses
+ * built in packages which we import and which we make for our own use
+ * 
+ * Access modifiers: fincance data should not be revealed to management or tech team , this is why we need access modifiers
+ *public - the function/datatype  can be accessed by everyone
+ default - it is default if we dont specify anything, our package can access it but a different package cannot
+ protected - can be accessed by own package but if someone wants to use it in another package, can only access subclasses nothing else
+private - can be accessed only inside a class not outside the class, not even subclass, and also not in other packages
+
+if we have to access private functions and datatypes we have to use setters and getters
+WATCH APNA COLLEGE OOP 1 SHOT 45:00
+*/
+
+
+
+
+
+
+
+public class Encapsulation {
+    
+}
