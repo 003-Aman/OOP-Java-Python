bf69ce6f647ea93914f92cd6648e4d0f2a019f64
Aman Shrestha <amanshrestha3003@gmail.com>
2025-11-21 21:29:20 -0500

    NOV 21

--- Polymorphism.py
+++ Polymorphism.py
@@ -28,6 +28,35 @@ p1 = Phone("Apple", "17 pro max")
 l1 = Laptop("Apple", "Macbook m4")
 w1 = Watch("Apple", "Series1")
 
-# Loop through and call info()
+# Loop through and call info().THIS ON IS INTERSTING HAVE A LOOK ON IT. USE LOOPING BETTER
 for i in (p1, l1, w1):
     i.info()
+
+#INHERITANCE CLASS POLYMORPHISM
+class Vehicle:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Move!")
+
+class Car(Vehicle): # FOR CLASS CAR THE MOVE FUNCTION WILL PRINT MOVE SINCE IT DOESNT HAVE ANYTHING OF ITS OWN . IT WILL INHERIT IT . BUT FOR THE OTHERS ITS NOT THE SAME BECAUSE THEY HAVE THE MOVE FUNCTION OF THEIR OWN
+  pass
+
+class Boat(Vehicle):
+  def move(self):
+    print("Sail!")
+
+class Plane(Vehicle):
+  def move(self):
+    print("Fly!")
+
+car1 = Car("Ford", "Mustang") #Create a Car object
+boat1 = Boat("Ibiza", "Touring 20") #Create a Boat object
+plane1 = Plane("Boeing", "747") #Create a Plane object
+
+for x in (car1, boat1, plane1):
+  print(x.brand)
+  print(x.model)
+  x.move()
